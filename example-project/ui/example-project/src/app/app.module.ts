577b732efb95bc1971d71be664778504e57933f4
sinanspd <sinanspd@gmail.com>
2020-06-03 23:55:34 -0400

    generate schemas dashboard

--- example-project/ui/example-project/src/app/app.module.ts
+++ example-project/ui/example-project/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule as CModule} from './common/common.module';
 import { ScalaphyDashboardComponent } from './scalaphy-dashboard/scalaphy-dashboard.component';
 import {MatIconModule} from '@angular/material/icon';
+import { SchemasComponent } from './schemas/schemas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScalaphyDashboardComponent
+    ScalaphyDashboardComponent,
+    SchemasComponent
   ],
   imports: [
     BrowserModule,
